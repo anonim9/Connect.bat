847c2b00811cff649014aa3e82413d2112a8f851
anonim9 <61760991+anonim9@users.noreply.github.com>
2020-04-09 11:40:41 +0300

    Update .bat

--- .bat
+++ .bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\OpenVPN\bin\openvpn-gui" --config_dir "C:\Users\user\OpenVPN\config\xxx" --connect axxx.ovpn
+"C:\Program Files\OpenVPN\bin\openvpn-gui" --config_dir "C:\Users\user\OpenVPN\config\xxx" --connect xxx.ovpn
